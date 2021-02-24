ae68c9facf5c075e99c872c2d1839a2163825d56
Roger-Maza <born_to_kill1998@outlook.com>
2021-02-23 20:39:48 -0500

    Modificacion python, agg html y agg css
    :wp

--- index.py
+++ index.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-	return render_template('homes.html')
+	return render_template('index.html')
 if __name__ == '__main__':
 	app.run()
